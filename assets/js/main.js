5267bd7f7b1b7c4beea2b57a23486ba261249b9c
FerFiguera <figuerafernanda@gmail.com>
2022-10-02 21:11:20 -0300

    ajustes y footer listo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
-git
+$(function () {
+    $('[data-bs-toggle="tooltip"]').tooltip();
+  });
+  
+  $(document).scroll(function () {
+    const y = $("html").scrollTop();
+  
+    y > 50 ? $("nav").addClass("nav-black") : $("nav").removeClass("nav-black");
+  });
